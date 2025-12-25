e89bd1f7b1c42cbfdb66162d153de316f6195bcf
Alex <alex.podikoglou@gmail.com>
2025-12-25 23:28:41 +0200

    feat: synthesize_instruction

--- optimizer.pl
+++ optimizer.pl
@@ -1,5 +1,37 @@
 ?-consult(vm).
 
+% synthesize_instruction/1
+%
+% generate all possible combinations of instructions
+
+synthesize_instruction(load(X)) :-
+  between(0, 255, X).
+
+synthesize_instruction(swap(A, B)) :-
+  between(0, 255, A),
+  between(0, 255, B).
+
+synthesize_instruction(xor(A, B)) :-
+  between(0, 255, A),
+  between(0, 255, B).
+
+synthesize_instruction(inc(X)) :-
+  between(0, 255, X).
+
+synthesize_instruction(dec(X)) :-
+  between(0, 255, X).
+
+synthesize_instruction(add(A, B)) :-
+  between(0, 255, A),
+  between(0, 255, B).
+
+synthesize_instruction(sub(A, B)) :-
+  between(0, 255, A),
+  between(0, 255, B).
+
+synthesize_instruction(put(X)) :-
+  between(0, 255, X).
+
 % synthesize_from_program/2
 %
 % P: input program
