977ad11eac3a2571b1638bf0ff00335741ef47a4
Alex <alex.podikoglou@gmail.com>
2025-12-25 23:38:35 +0200

    refactor: synthesize for multiple lengths

--- optimizer.pl
+++ optimizer.pl
@@ -45,13 +45,22 @@ synthesize_from_program(P, O) :-
 % P:  input program
 % O:  output program
 synthesize_from_program(IM, P, O) :-
+  % this is probably computed multiple times, sucks
   execute_program(IM, P, TM),
-  synthesize(IM, TM, O).
 
-% synthesize/3
+  % generate all target lengths ([0..L] where L is the length of the input
+  % program)
+  length(P, L),
+  between(0, L, TL),
+
+  % synthesize for TL
+  synthesize(IM, TM, TL, O).
+
+% synthesize/4
 %
 % IM: initial memory
 % TM: target memory
+% TL: target length (amount of instruction)
 % O:  output program
-synthesize(IM, TM, O) :-
+synthesize(IM, TM, TL, O) :-
   true.
