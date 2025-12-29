17e1401cfaf5ca5ccb4b722b3265a6b175cb8270
Alex <alex.podikoglou@gmail.com>
2025-12-29 20:44:16 +0200

    refactor: move instruction/1 to isa.pl

--- optimizer.pl
+++ optimizer.pl
@@ -1,44 +1,6 @@
 ?-consult(vm).
+?-consult(isa).
 
-% instruction/1
-%
-% Has three uses:
-% 1) Checking the validity of an instruction (when given an instruction as the
-%    argument)
-%
-% 2) Generating all possible arguments an instruction can be given (when given
-%    an instruction name with variables in the place of its arguments)
-%
-% 3) Generating all possible instructions (when given a variable in the place
-%    of its argument)
-
-instruction(load(X)) :-
-  between(0, 255, X).
-
-instruction(swap(A, B)) :-
-  between(0, 255, A),
-  between(0, 255, B).
-
-instruction(xor(A, B)) :-
-  between(0, 255, A),
-  between(0, 255, B).
-
-instruction(inc(X)) :-
-  between(0, 255, X).
-
-instruction(dec(X)) :-
-  between(0, 255, X).
-
-instruction(add(A, B)) :-
-  between(0, 255, A),
-  between(0, 255, B).
-
-instruction(sub(A, B)) :-
-  between(0, 255, A),
-  between(0, 255, B).
-
-instruction(put(X)) :-
-  between(0, 255, X).
 
 % synthesize_from_program/2
 %
